0b46c19726c73264e8075622d39dab582bc8ab06
elishaobot8-coder <eobot@byupathway.edu>
2026-08-05 12:30:09 +0100

    just

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const job = new cron.CronJob("*/14 * * * *", function () {
+const job = new cron.CronJob("* * * * *", function () {
  // Replace with your desired URL
 
   https.get(`${process.env.API_URL}/api/test`, (res) => {
