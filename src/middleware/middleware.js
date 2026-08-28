568ca5705a1b12345b678bdfa149e502ad8b76b7
elishaobot8-coder <eobot@byupathway.edu>
2026-08-28 07:42:02 +0100

    jwt update

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,37 +1,38 @@
-import jwt from "jsonwebtoken"
-import UserModel from "../models/userModel.js"
-const secret = process.env.JWT_SECRET
+import jwt from "jsonwebtoken";
+import UserModel from "../models/userModel.js";
 
+const secret = process.env.JWT_SECRET;
 
-
-
-const protectRoutes = async (req, res, next)=>{
-
+const protectRoutes = async (req, res, next) => {
   try {
-    
-   const token = req.headers.authorization?.replace("Bearer ", "");
+    const token = req.headers.authorization?.replace("Bearer ", "");
+
     if (!token) {
-      return res.status(401).json({message:"unauthorized user"})
+      return res.status(401).json({
+        message: "unauthorized user"
+      });
     }
-   
+
     const decodeToken = jwt.verify(token, secret);
 
-    const user= await UserModel.findById(decodeToken.userid).select("-password")
-   
-    if(!user){
-      return res.status(401).json({message:"token is invalid"})
+    const user = await UserModel.findById(decodeToken.userid)
+      .select("-password");
+
+    if (!user) {
+      return res.status(401).json({
+        message: "token is invalid"
+      });
     }
 
-    req.user= user
+    req.user = user;
 
-    next()
+    next();
 
   } catch (error) {
-
-    console.log(error.message)
-    res.status(401).json(error.message);
-  
+    res.status(401).json({
+      message: error?.message || String(error)
+    });
   }
-}
+};
 
-export default protectRoutes
+export default protectRoutes;
