6565d8f87ae1cb04e3c3d0e1a92adfff654b66db
elishaobot8-coder <eobot@byupathway.edu>
2026-09-09 11:11:47 +0100

    cronjob

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -1,8 +1,61 @@
 import cron from "cron";
 import https from "https";
+import UserModel from "../models/userModel.js";
+import HistoryModel from "../models/historyModel.js";
 
-const job = new cron.CronJob("* * * * *", function () {
+
+const job = new cron.CronJob("* * * * *", async function () {
+
+  console.log("cron job is running")
+
+const fiveMinutesAgo = new Date(
+  Date.now()- 5*60*100
+);
+
+const pendingTransfer= await HistoryModel.find({
+  status:"..pending",
+  date:{
+    $lte: fiveMinutesAgo
+  }
+});
+
+for(const transfer of pendingTransfer){
+  try {
+    const userId= transfer.user;
+
+    const updatUser= await UserModel.findByIdAndUpdate(userId, {
+      $inc:{
+        balance: transfer.amount
+      }
+    },
+  
+  {
+    new:true
+  });
+
+  if(!updatUser){
+    console.log(`user not found for transfer ${transfer._id}`);
+continue;
+  }
+
+  await HistoryModel.findByIdAndUpdate(transfer._id,{
+    status:"reversed"
+  });
+
+  console.log(`Transfer ${transfer._id} reversed.  money returned to user ${userId}`);
+
+
+  } catch (error) {
+    console.log(`Error reversing transfer ${transfer._id}: ${error.message}`);
+  }
+}
+
+
+
+
  // Replace with your desired URL
+
+
 
   https.get(`${process.env.API_URL}/api/test`, (res) => {
     if(res.statusCode=== 200){console.log(`Cron job executed. Status code: ${res.statusCode}`);}
